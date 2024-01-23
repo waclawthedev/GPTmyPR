75fa25f39ad7ff957134c012c073e827c753d1a6
admin <admin@admin.local>
2024-01-23 23:19:49 +0100

    setup.py improvements

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="gptmypr",
     version="0.1.0",
     author="waclawthedev",
     author_email="waclawthedev@gmail.com",
     url="https://github.com/waclawthedev/GPTmyPR",
-    description="CLI tool to improve the PR after code review using OpenAI GPT",
-    long_description="CLI tool to improve the PR after code review using OpenAI GPT",
+    description="CLI tool that uses OpenAI's GPT to change your code according to comments in the GitHub pull request",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     packages=find_packages(),
     entry_points={"console_scripts": ["gptmypr = gptmypr.gptmypr:main"]},
-    keywords="gptmypr",
-    install_requires=["rich==13.7.0", "openai==1.9.0", "PyGithub==2.1.1", "GitPython==3.1.41"],
+    keywords=[
+        "gptmypr",
+        "pull request",
+        "code review",
+        "refactoring",
+        "code quality",
+        "ChatGPT",
+        "GPT",
+        "OpenAI",
+        "GitHub",
+    ],
+    install_requires=[
+        "rich==13.7.0",
+        "openai==1.9.0",
+        "PyGithub==2.1.1",
+        "GitPython==3.1.41",
+    ],
     zip_safe=False,
 )
